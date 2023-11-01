01df66310aaf2a684e83c6f05515e3bd64a21a96
Vitor Andrey <vitor.andreylopess@gmail.com>
2023-11-01 07:56:40 -0300

    chore: add the query-client-provider

--- src/components/layout/AppProvider.tsx
+++ src/components/layout/AppProvider.tsx
@@ -7,16 +7,21 @@ import { View } from "react-native";
 import { UserContextProvider } from "@contexts/UserContext";
 import { ShoppingListProvider } from "@contexts/ShoppingList";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
 export function AppProvider({ children }: { children: React.ReactNode }) {
   return (
     <View className="flex-1">
-      <UserContextProvider>
-        <ShoppingListProvider>
-          <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-            {children}
-          </SafeAreaProvider>
-        </ShoppingListProvider>
-      </UserContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <UserContextProvider>
+          <ShoppingListProvider>
+            <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+              {children}
+            </SafeAreaProvider>
+          </ShoppingListProvider>
+        </UserContextProvider>
+      </QueryClientProvider>
     </View>
   );
 }
