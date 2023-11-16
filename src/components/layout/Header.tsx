fe7b0253b4ad7f72f8acbf71e3970e3d727e4467
Marcella <129318421+marcella28@users.noreply.github.com>
2023-11-16 16:54:57 -0300

    alt ui ux

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,7 +9,7 @@ export function Header({ className, ...rest }: HeaderProps) {
   return (
     <View
       className={twMerge(
-        "h-12 flex-row items-center justify-center gap-1",
+        "h-12 flex-row items-center justify-center gap-1 mt-8",
         className,
       )}
       {...rest}
